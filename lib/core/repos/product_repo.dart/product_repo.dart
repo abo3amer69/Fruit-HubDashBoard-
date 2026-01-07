0e4d297b726dae4a1de6f60f4a69f9299ed0020d
Ahmed Amer <AhmedAmer>
2026-01-07 14:03:11 +0200

    create images and product repo

---
+++ lib/core/repos/product_repo.dart/product_repo.dart
@@ -0,0 +1,6 @@
+import 'package:fruits_apps_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
+
+abstract class ProductRepo {
+    Future <void> addProduct(AddProductInputEntity addProductInputEntity);
+    
+}
---
+++ lib/core/repos/product_repo.dart/product_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:fruits_apps_dashboard/core/repos/product_repo.dart/product_repo.dart';
+import 'package:fruits_apps_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
+
+class ProductRepoImpl implements ProductRepo{
+  @override
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity) {
+    // TODO: implement addProduct
+    throw UnimplementedError();
+  }
+}
